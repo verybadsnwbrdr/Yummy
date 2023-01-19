feb0d26afa9281bd397cd163974111ca579f42aa
Anton Khrapov <froe054@mail.ru>
2023-01-19 21:07:34 +0700

    Initial Commit

---
+++ Вкусно?/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Вкусно?
+//
+//  Created by Anton on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
